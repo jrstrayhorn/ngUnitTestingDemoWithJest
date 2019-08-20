ed1fae004bc357eb0225179bb5622e18e24aa33d
J.R. Strayhorn <jrstrayhorn@gmail.com>
2019-08-20 10:54:52 -0400

    done with testing interactions

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,6 +1,8 @@
 import { HeroesComponent } from './heroes.component';
 import { of } from 'rxjs';
 
+// Isolated Tests - not using Angular to initial/render/compile component
+// just testing straight up HeroesComponent JavaScript/TypeScript class
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let HEROES;
@@ -23,18 +25,31 @@ describe('HeroesComponent', () => {
   });
 
   describe('delete', () => {
+    // State Change Based Test
+    // testing that the state of the component changed
     it('should remove the indicated hero from the heroes list', () => {
       // How to create mock return value in Jasmine
       //mockHeroService.deleteHero.and.returnValue(of(true));
 
       // How to create mock return value in Jest
       mockHeroService.deleteHero.mockReturnValue(of(true));
-
       component.heroes = HEROES;
 
       component.delete(HEROES[2]);
 
       expect(component.heroes.length).toBe(2);
+    });
+
+    // Interaction Based Test
+    // testing that the component properly called the collaborator (in this case, the service)
+    it('should call deleteHero with correct hero', () => {
+      mockHeroService.deleteHero.mockReturnValue(of(true));
+      component.heroes = HEROES;
+      // we could also call component.ngOnInit() to initialize if we want to test that
+
+      component.delete(HEROES[2]);
+
+      expect(mockHeroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
     });
   });
 });
